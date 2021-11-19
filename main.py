74066b65232967d8fc338fdeb34cdae35e435f70
jakaBlaz <blaz.molicnik@gmail.com>
2021-11-19 15:06:05 +0100

    femza first test

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
 from datetime import datetime as dt
 
+#Global Variables and Dictionaries
+day_of_the_week = {
+    "Monday":"ponedeljek",
+    "Tuesday":"torek",
+    "Wednesday":"sreda",
+    "Thursday":"četrtek",
+    "Friday":"petek",
+    "Saturday":"sobota",
+    "Sunday":"nedelja",
+}
+
+menu = dict()
+
+# Functions
 def marjetica():
     r = requests.get("https://marjetice.si/")
     mainBody = r.text
@@ -13,4 +27,25 @@ def marjetica():
     #table_with_food = table_with_food[table_with_food.find("<table>"):table_with_food.find("</table>")]
     return table_with_food
 
-print(marjetica())
+
+def femza():
+    r = requests.get("https://fe.uni-lj.si/o_fakulteti/restavracija/tedenski_meni/",verify=False)
+    mainBody = r.text
+    mainBody = mainBody[mainBody.find("<body"):mainBody.find("</body")]
+    temporarySearchalg = mainBody# [mainBody.find('cnt-1808'):mainBody.find("</div>",mainBody.find('cnt-1808'))]
+    datum = dt.now().strftime("%d.%m.%Y")
+    dan = day_of_the_week[dt.now().strftime("%A")]
+    temporarySearchalg = temporarySearchalg[temporarySearchalg.find("<table",temporarySearchalg.find(dan)):temporarySearchalg.find("</table>",temporarySearchalg.find(dan))]
+    temporarySearchalg = temporarySearchalg.split("<tr>")[1:]
+    i = 0
+    for i in len(temporarySearchalg):
+        temporarySearchalg[i] = temporarySearchalg[i].strip("</tr>").strip("</td>")
+    
+    print(temporarySearchalg)
+    print(len(temporarySearchalg))
+    # table_with_food = temporarySearchalg.split("<br />")[1:]
+    #table_with_food = table_with_food[table_with_food.find("<table>"):table_with_food.find("</table>")]
+    return 0
+    
+print(femza())
+
