6c963031b67deda2942a4305d3728b9ab3156f03
jakaBlaz <blaz.molicnik@gmail.com>
2023-03-24 19:11:15 +0100

    some new version to seem busy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from datetime import datetime as dt
 
 #Global Variables and Dictionaries
@@ -24,28 +25,24 @@ def marjetica():
     temporarySearchalg = temporarySearchalg[(temporarySearchalg.find(datum)+ len( datum+"<strong>")+1):temporarySearchalg.find("</p>",temporarySearchalg.find(datum))]
 
     table_with_food = temporarySearchalg.split("<br />")[1:]
-    #table_with_food = table_with_food[table_with_food.find("<table>"):table_with_food.find("</table>")]
+    for i in range(len(table_with_food)):
+        table_with_food[i] = table_with_food[i].split(",")
     return table_with_food
 
 
 def femza():
-    r = requests.get("https://fe.uni-lj.si/o_fakulteti/restavracija/tedenski_meni/",verify=False)
-    mainBody = r.text
-    mainBody = mainBody[mainBody.find("<body"):mainBody.find("</body")]
-    temporarySearchalg = mainBody# [mainBody.find('cnt-1808'):mainBody.find("</div>",mainBody.find('cnt-1808'))]
-    datum = dt.now().strftime("%d.%m.%Y")
-    dan = day_of_the_week[dt.now().strftime("%A")]
-    temporarySearchalg = temporarySearchalg[temporarySearchalg.find("<table",temporarySearchalg.find(dan)):temporarySearchalg.find("</table>",temporarySearchalg.find(dan))]
-    temporarySearchalg = temporarySearchalg.split("<tr>")[1:]
-    i = 0
-    for i in len(temporarySearchalg):
-        temporarySearchalg[i] = temporarySearchalg[i].strip("</tr>").strip("</td>")
-    
-    print(temporarySearchalg)
-    print(len(temporarySearchalg))
-    # table_with_food = temporarySearchalg.split("<br />")[1:]
-    #table_with_food = table_with_food[table_with_food.find("<table>"):table_with_food.find("</table>")]
-    return 0
-    
+    table_with_food = pd.read_html('https://www.fe.uni-lj.si/o_fakulteti/restavracija/tedenski_meni/')
+    datum = dt.now().strftime("%w")
+    if int(datum) > 0:
+        datum = int(datum) - 1
+    else:
+        datum = 6
+    # print(datum)
+    # print(table_with_food[datum])
+    return table_with_food[datum]
+
+
+
+print(marjetica())
 print(femza())
 
